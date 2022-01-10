26c8233f40d53c8598a9807df418089e01e200ab
cecile.chow@gmail.com <cecile.chow@gmail.com>
2022-01-10 22:26:40 +0000

    add daily_total_by_payment.sql in analysis folder

---
+++ analysis/daily_total_by_payment_status.sql
@@ -0,0 +1,25 @@
+with payments as (
+
+    select * from {{ ref('stg_payments') }}
+
+),
+
+daily_total as (
+
+    select
+        created_at,
+        sum(amount) as total_possible_revenue,
+        
+    {% for status in ['success', 'fail'] %}
+
+        sum(case when status = '{{ status }}' then amount else 0 end) as {{ status }}_total {{ ',' if not loop.last}}
+
+    {% endfor %}
+
+    from payments
+    group by 1
+    order by 1
+
+)
+
+select * from daily_total
