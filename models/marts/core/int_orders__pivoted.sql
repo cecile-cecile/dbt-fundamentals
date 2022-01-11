ad4fa9d7ca315935fa7a2012055eb986009a07e2
cecile.chow@gmail.com <cecile.chow@gmail.com>
2022-01-11 01:45:22 +0000

    fix orders_by_day.sql formatting around jinja

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -9,7 +9,7 @@ pivoted as (
     select
         order_id,
         {% for payment_method in payment_methods -%}
-        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount {{ ',' if not loop.last}}
+        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount{{',' if not loop.last}}
         {% endfor %}
     from payments
     where status = 'success'
